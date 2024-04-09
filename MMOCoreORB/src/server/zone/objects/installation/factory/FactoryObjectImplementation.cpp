b5a894985a9ae99637b2cc81a5556933931ea441
mannax2012 <mannax@DESKTOP-MUVVHM7>
2024-04-09 00:11:49 -0500

    [TESTING] Factory and Craft Timers Instant

--- MMOCoreORB/src/server/zone/objects/installation/factory/FactoryObjectImplementation.cpp
+++ MMOCoreORB/src/server/zone/objects/installation/factory/FactoryObjectImplementation.cpp
@@ -566,7 +566,7 @@ bool FactoryObjectImplementation::startFactory() {
 	timer = 30;
 	info(true) << "Factory Testing Timer Set To: " << timer;
 #else
-	timer = ((int)schematic->getComplexity()) * 8;
+	timer = ((int)schematic->getComplexity()) * 0.01;
 #endif
 
 	if (!populateSchematicBlueprint(schematic))
