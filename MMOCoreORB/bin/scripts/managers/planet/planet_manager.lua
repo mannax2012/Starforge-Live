865089b40bdc9bf7b203f92509b56c1eaef7e123
mannax2012 <mannax@DESKTOP-MUVVHM7>
2024-04-21 16:11:17 -0500

    [UPDATE] Resource Names and Shuttle Times

--- MMOCoreORB/bin/scripts/managers/planet/planet_manager.lua
+++ MMOCoreORB/bin/scripts/managers/planet/planet_manager.lua
@@ -41,16 +41,16 @@
 --which carries forward this exception.
 
 --Time, in seconds, that a shuttle is away for
-shuttleportAwayTime = 300
-starportAwayTime = 60
+shuttleportAwayTime = 10
+starportAwayTime = 10
 
 --Time, in seconds, that a shuttle takes to land
-shuttleportLandingTime = 11
-starportLandingTime = 14
+shuttleportLandingTime = 1
+starportLandingTime = 1
 
 --Time, in seconds, that a shuttle is landed for
-shuttleportLandedTime = 120
-starportLandedTime = 120
+shuttleportLandedTime = 86400
+starportLandedTime = 86400
 
 
 corellia = {
