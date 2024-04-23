748399d753b97316412232ff85789bbcdf920320
mannax2012 <mannax@DESKTOP-MUVVHM7>
2024-04-22 20:23:38 -0500

    [UPDATED] More factoryCrateSize = 1000

--- MMOCoreORB/bin/scripts/object/draft_schematic/clothing/component/clothing_fiberplast_panel.lua
+++ MMOCoreORB/bin/scripts/object/draft_schematic/clothing/component/clothing_fiberplast_panel.lua
@@ -52,7 +52,7 @@ object_draft_schematic_clothing_component_clothing_fiberplast_panel = object_dra
 	size = 2,
 	factoryCrateSize = 50,
 	factoryCrateType = "object/factory/factory_crate_clothing.iff",
-
+	factoryCrateSize = 1000,
 	xpType = "crafting_general",
 	xp = 20,
 
--- MMOCoreORB/bin/scripts/object/draft_schematic/clothing/component/clothing_reinforced_fiber_panels.lua
+++ MMOCoreORB/bin/scripts/object/draft_schematic/clothing/component/clothing_reinforced_fiber_panels.lua
@@ -51,7 +51,7 @@ object_draft_schematic_clothing_component_clothing_reinforced_fiber_panels = obj
    complexity = 15, 
    size = 1, 
    factoryCrateType = "object/factory/factory_crate_clothing.iff",
-
+   factoryCrateSize = 1000,
    xpType = "crafting_clothing_general", 
    xp = 75, 
 
