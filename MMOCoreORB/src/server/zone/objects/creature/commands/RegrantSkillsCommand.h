81147d03de04ee4a4c5529dfacaa9c7f6f812661
mannax2012 <mannax@GamePC01>
2024-09-15 23:09:02 -0500

    [UPDATED] RegrantSkills - Now will not remove FRS or Regrant FRS if removed.

--- MMOCoreORB/src/server/zone/objects/creature/commands/RegrantSkillsCommand.h
+++ MMOCoreORB/src/server/zone/objects/creature/commands/RegrantSkillsCommand.h
@@ -28,20 +28,22 @@ public:
 		// This command only works on the player executing the command
 
 		Locker locker(creature);
-
+		TransactionLog trx(TrxCode::SKILLTRAININGSYSTEM, creature);
+		ManagedReference<PlayerObject*> ghost = creature->getPlayerObject();
 		SkillManager* skillManager = SkillManager::instance();
 		const SkillList* skillList = creature->getSkillList();
+		int frsXP = ghost->getExperience("force_rank_xp");
 
 		if (skillList == nullptr)
 			return GENERALERROR;
 
-
+		
 				if (!creature->checkCooldownRecovery("regrantSkills")) {
 							creature->sendSystemMessage("You cannot have your skill regranted more than once per 12 hours.");
 							return GENERALERROR;
 						}
 				creature->updateCooldownTimer("regrantSkills", 43200000);
-
+		
 		String skillName = "";
 		Vector<String> listOfNames;
 		skillList->getStringList(listOfNames);
@@ -101,6 +103,22 @@ public:
 			}
 		}
 
+		if (creature->hasSkill("force_title_jedi_rank_03")){
+			if (ghost->getExperience("force_rank_xp") <= 0){
+
+				creature->sendSystemMessage("Completed. Force Rank Experience has been set to: " + String::valueOf(frsXP));
+
+				TransactionLog trxExperience(TrxCode::EXPERIENCE, creature);
+				trxExperience.groupWith(trx);
+				ghost->addExperience(trxExperience, "force_rank_xp", frsXP, true);
+
+			}else{
+				creature->sendSystemMessage("Completed. Force Rank Experience has remained unchanged: " + String::valueOf(frsXP));
+			}
+		}else{
+			creature->sendSystemMessage("Completed.");
+		}
+		
 		return SUCCESS;
 	}
 
